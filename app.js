35a3752e5ea10be27e036600a938ca27de87e159
Evandro Mathias <119955079+MathiasDev-stack@users.noreply.github.com>
2025-01-24 01:38:39 -0300

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+alert('boas vindas ao jogo do numero secreto');
+let numerosecreto =5;
+let chute = prompt('escolha um numero entre 1 e 10')
+if (numerosecreto == chute) {
+    console.log('isso ai voce acertou o numero secreto')
+}
